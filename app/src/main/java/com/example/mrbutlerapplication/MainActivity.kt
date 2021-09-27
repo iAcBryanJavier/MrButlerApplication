1c0641a955484abcca3f1a6029a7a9fcc79a20f3
Bryan Javier <201901165@iacademy.edu.ph>
2021-09-27 18:05:38 +0800

    Changes made: * activity_main.xml Layout changed from Constraint to Relative and Linear * login_page.xml Submit and Reset button fixed * added EmailAdds.kt for email addresses of butlers

--- app/src/main/java/com/example/mrbutlerapplication/MainActivity.kt
+++ app/src/main/java/com/example/mrbutlerapplication/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import com.example.mrbutlerapplication.authentication.EmailAdds
 import com.example.mrbutlerapplication.authentication.LoginPage
 class MainActivity : AppCompatActivity() {
 
     //initialize control variables
     private lateinit var username1: TextView
     private lateinit var username2: TextView
+    private lateinit var username3: TextView
+    private lateinit var username4: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,16 +23,22 @@ class MainActivity : AppCompatActivity() {
         //set control variables
         username1 = findViewById(R.id.username_1)
         username2 = findViewById(R.id.username_2)
+        username3 = findViewById(R.id.username_3)
+        username4 = findViewById(R.id.username_4)
 
     }
 
     fun login(view: View){
-        //displays the view id (for testing purposes only)
+        //initializes Email Address class to access employee email addresses
+        var user: EmailAdds = EmailAdds()
 
+        //displays the view id (for testing purposes only)
         Toast.makeText( this,"View ID: ${view.resources.getResourceEntryName(view.id)}", Toast.LENGTH_SHORT).show()
         when{
-            view.resources.getResourceEntryName(view.id) == "loginBtn1" -> switchActivity(username1.text.toString(), username1.hint.toString())
-            view.resources.getResourceEntryName(view.id) == "loginBtn2" -> switchActivity(username2.text.toString(), username2.hint.toString())
+            view.resources.getResourceEntryName(view.id) == "loginBtn1" -> switchActivity(username1.text.toString(), user.butler1)
+            view.resources.getResourceEntryName(view.id) == "loginBtn2" -> switchActivity(username2.text.toString(), user.butler2)
+            view.resources.getResourceEntryName(view.id) == "loginBtn3" -> switchActivity(username3.text.toString(), user.butler3)
+            view.resources.getResourceEntryName(view.id) == "loginBtn4" -> switchActivity(username4.text.toString(), user.butler4)
         }
     }
 
---
+++ app/src/main/java/com/example/mrbutlerapplication/authentication/EmailAdds.kt
@@ -0,0 +1,10 @@
+package com.example.mrbutlerapplication.authentication
+
+class EmailAdds {
+    public var butler1: String = "bryantrijavier@gmail.com"
+    public var butler2: String = "ivanlee@gmail.com"
+    public var butler3: String = "rjjalosjos@gmail.com"
+    public var butler4 : String = "patrickcalling@gmail.com"
+
+    constructor()
+}
