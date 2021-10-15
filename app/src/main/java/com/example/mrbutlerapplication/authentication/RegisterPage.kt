66dc87704c90c1428f7ce7242237d672e89977b8
Bryan Javier <201901165@iacademy.edu.ph>
2021-10-15 16:44:47 +0800

    Employee.kt is can now be uploaded to the realtime database

--- app/src/main/java/com/example/mrbutlerapplication/authentication/RegisterPage.kt
+++ app/src/main/java/com/example/mrbutlerapplication/authentication/RegisterPage.kt
@@ -6,22 +6,29 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.mrbutlerapplication.R
+import com.example.mrbutlerapplication.model.Employee
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
 class RegisterPage : AppCompatActivity() {
 
     private lateinit var fullName: TextInputEditText
     private lateinit var phoneNum: TextInputEditText
-    private lateinit var email:    TextInputEditText
+    private lateinit var email: TextInputEditText
     private lateinit var password: TextInputEditText
+    private lateinit var age: TextInputEditText
+    private lateinit var address: TextInputEditText
+
 
     private lateinit var authentication: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,28 +38,35 @@ class RegisterPage : AppCompatActivity() {
         phoneNum = findViewById(R.id.phoneNum)
         email = findViewById(R.id.emailAdd)
         password = findViewById(R.id.password)
+        age = findViewById(R.id.age)
+        address = findViewById(R.id.address)
 
+        //firebase database initialization
         authentication = FirebaseAuth.getInstance()
-
-
+        database = Firebase.database
 
 
     }
 
     fun register(view: View?) {
-        registerUser(fullName.text.toString(), phoneNum.text.toString(), email.text.toString(), password.text.toString())
+        registerUser(fullName.text.toString(), phoneNum.text.toString(), email.text.toString(), password.text.toString(), age.text.toString(), address.text.toString())
     }
 
-    private fun registerUser(name: String, phone: String, email: String, password: String){
+    private fun registerUser(name: String, phone: String, email: String, password: String, age:String, address: String){
         var intent: Intent = Intent(this@RegisterPage, LoginPage::class.java)
         intent.putExtra("USERNAME", "Welcome ${name} to Mr. Butler!")
         intent.putExtra("EMAIL", email)
 
+
         authentication.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this@RegisterPage, object: OnCompleteListener<AuthResult>{
             override fun onComplete(p0: Task<AuthResult>) {
                 if(p0.isSuccessful){
-                    startActivity(intent)
                     Toast.makeText(this@RegisterPage, "User Registered Successfully!",Toast.LENGTH_SHORT).show()
+                    var employee = Employee(name, email, phone,age,address)
+                    var ref = database.getReference("Employee")
+                    ref.child(authentication.uid.toString()).setValue(employee)
+
+                    startActivity(intent)
                 }else{
                     Toast.makeText(this@RegisterPage, "Please Check Fields! Try Again.", Toast.LENGTH_SHORT).show()
                 }
--- app/src/main/java/com/example/mrbutlerapplication/model/Employee.kt
+++ app/src/main/java/com/example/mrbutlerapplication/model/Employee.kt
@@ -1,11 +1,17 @@
 package com.example.mrbutlerapplication.model
 
 class Employee {
-    var username: String = ""
-    var password: String = ""
+    var name: String = ""
+    var email: String = ""
+    var contactNum: String = ""
+    var age: String = ""
+    var address: String = ""
 
-    constructor(username: String, password: String){
-        this.username = username
-        this.password = password
+    constructor(name: String, email: String, contactNum: String, age: String, address: String){
+        this.name = name
+        this.email = email
+        this.contactNum = contactNum
+        this.age = age
+        this.address = address
     }
 }
