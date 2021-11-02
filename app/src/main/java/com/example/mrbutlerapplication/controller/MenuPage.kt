1f5614bf97dd6222ed4a1411aa86dd32a1bd669d
Richard Lee <201901294@iacademy.edu.ph>
2021-11-03 00:11:00 +0800

    profile page created and connected to menu page

--- app/src/main/java/com/example/mrbutlerapplication/controller/MenuPage.kt
+++ app/src/main/java/com/example/mrbutlerapplication/controller/MenuPage.kt
@@ -1,7 +1,10 @@
 package com.example.mrbutlerapplication.controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.example.mrbutlerapplication.ButlerProfileCard
 import com.example.mrbutlerapplication.R
 
 class MenuPage : AppCompatActivity() {
@@ -9,6 +12,13 @@ class MenuPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu_page)
 
+        val  profile = findViewById<ImageView>(R.id.profile)
+
+        profile.setOnClickListener{
+
+            val Intent = Intent(this,ButlerProfileCard::class.java)
+            startActivity(Intent)
+        }
 
 
 
